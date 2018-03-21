c706dcd27425d1ac963bb2cb4ce4ec4929956442
Aaron Dawson <ardwsn@gmail.com>
2018-03-21 14:25:37 +0000

    Delete connect.ps1

--- 365/connect.ps1
+++
@@ -1,3 +0,0 @@
-$365Credential = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $365Credential -Authentication Basic -AllowRedirection
-Import-PSSession $Session
