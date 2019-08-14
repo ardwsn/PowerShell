0b4a40fb276ee77f28d418abf7830c4fd1a2c3ee
Aaron Dawson <ardwsn@gmail.com>
2019-08-14 10:29:03 +0100

    Create disable-group-creation.ps1

---
+++ Azure AD/disable-group-creation.ps1
@@ -0,0 +1,20 @@
+#It is recommended to uninstall the AzureAD PowerShell module, and replace with AzureADPreview.
+#If you have both installed, modify Connect-AzureAD to AzureADPreview\Connect-AzureAD
+
+$AllowGroupCreation = "False"
+
+Connect-AzureAD
+
+$settingsObjectID = (Get-AzureADDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).id
+if(!$settingsObjectID)
+{
+	  $template = Get-AzureADDirectorySettingTemplate | Where-object {$_.displayname -eq "group.unified"}
+    $settingsCopy = $template.CreateDirectorySetting()
+    New-AzureADDirectorySetting -DirectorySetting $settingsCopy
+    $settingsObjectID = (Get-AzureADDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).id
+}
+
+$settingsCopy = Get-AzureADDirectorySetting -Id $settingsObjectID
+$settingsCopy["EnableGroupCreation"] = $AllowGroupCreation
+
+Set-AzureADDirectorySetting -Id $settingsObjectID -DirectorySetting $settingsCopy
